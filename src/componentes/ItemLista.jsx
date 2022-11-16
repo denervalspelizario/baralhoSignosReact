16e1dfb2f09deb1a67d02e0de962b53731103d35
denervalspelizario <denervalspelizario@gmail.com>
2022-11-16 09:54:10 -0300

    update projeto baralhoSignos estilizacao componente itemlist com hover

--- src/componentes/ItemLista.jsx
+++ src/componentes/ItemLista.jsx
@@ -4,11 +4,11 @@ import '../styles/componentes/ItemLista.sass';
 
 function ItemLista() {
     return (
-      <div className="box__sino">
+      <div className="box__signo">
         <div className="box__signo__nome">
           <p>Aquário</p> 
         </div>
-        <img className='box__signo__image' src="" alt="Aquário" />
+        <img className='box__signo__image' src="../../public/assets/aquario.jpg" alt="Aquário" />
         <div className="box__signo__rage">
           <p>21/01 - 19/02</p>
         </div>
