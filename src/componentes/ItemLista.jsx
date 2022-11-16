6e041c8e1f1d71380b6f7e9f331f38bcba057357
denervalspelizario <denervalspelizario@gmail.com>
2022-11-16 09:11:55 -0300

    update projeto baralhoSignos html componente itemlista

--- src/componentes/ItemLista.jsx
+++ src/componentes/ItemLista.jsx
@@ -4,9 +4,14 @@ import '../styles/componentes/ItemLista.sass';
 
 function ItemLista() {
     return (
-      <div className="Titulo">
-        <h1>Item lista</h1>
-  
+      <div className="box__sino">
+        <div className="box__signo__nome">
+          <p>Aquário</p> 
+        </div>
+        <img className='box__signo__image' src="" alt="Aquário" />
+        <div className="box__signo__rage">
+          <p>21/01 - 19/02</p>
+        </div>
       </div>
     )
   }
